ea9d907af7b96a6828ef0d8cd11f4d4e0efe7b35
Kevin Zhang <kevinctzhang@gmail.com>
2016-09-03 15:05:34 -0700

    swift Q

---
+++ SwiftQueue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftQueue
+//
+//  Created by Kevin Zhang on 2016-09-03.
+//  Copyright © 2016 Kevin Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
